74d721673d4bc8be1ad26d6219f51471bca319a3
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2023-03-21 15:50:20 +0000

    updating vault plugin to latest version

--- shipyard/vault/files/get-plugins.sh
+++ shipyard/vault/files/get-plugins.sh
@@ -2,9 +2,9 @@
 
 if [ ! -f /files/vault_plugins/boundary ]; then
   mkdir /files/vault_plugins
-  curl -L -o boundary.zip https://github.com/hashicorp-dev-advocates/vault-plugin-boundary-secrets-engine/releases/download/v1.0.0/vault-plugin-boundary-secrets-engine_v1.0.0_linux_amd64.zip
+  curl -L -o boundary.zip https://github.com/hashicorp-dev-advocates/vault-plugin-boundary-secrets-engine/releases/download/v1.0.2/vault-plugin-boundary-secrets-engine_v1.0.2_linux_amd64.zip
 
   unzip boundary.zip -d .
 
-  mv vault-plugin-boundary-secrets-engine_v1.0.0 /files/vault_plugins/boundary
+  mv vault-plugin-boundary-secrets-engine_v1.0.2 /files/vault_plugins/boundary
 fi
